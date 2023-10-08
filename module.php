70997f709198b6a26c7f1c7c7f40d474c2e88649
ommp-app <contact@ommp.app>
2023-10-08 19:35:46 +0200

    Add module page

--- module.php
+++ module.php
@@ -52,6 +52,34 @@ function store_delete_user($id) {
  *      FALSE if the action does not exists
  */
 function store_process_api($action, $data) {
+    global $user, $sql, $db_prefix;
+
+    if ($action == "get-module-versions") {
+
+        // Check the parameters
+		if (!check_keys($data, ["id"])) {
+			return ["error" => $user->module_lang->get("missing_parameter")];
+		}
+
+        $versions = [];
+        $request = $sql->query("SELECT version, required, timestamp FROM {$db_prefix}store_versions, {$db_prefix}store_modules WHERE name = " . $sql->quote($data['id']) . " AND id = store_id AND NOT hidden AND NOT blocked ORDER BY version DESC");
+        while ($version = $request->fetch()) {
+            $versions[] = [
+                "version" => $version['version'],
+                "required" => $version['required'],
+                "timestamp" => $version['timestamp'],
+                "formatted_timestamp" => date($user->module_lang->get("date_format"), $version['timestamp']),
+            ];
+        }
+        $request->closeCursor();
+
+        return [
+            "ok" => TRUE,
+            "versions" => $versions
+        ];
+
+    }
+
     return FALSE;
 }
 
@@ -77,6 +105,14 @@ function store_process_page($page, $pages_path) {
     global $user, $db_prefix, $sql;
     $data_path = module_get_data_path("store");
 
+    // Check if we must save a version and return page
+    if (isset($_GET['version'])) {
+        set_ommp_cookie("store.required_version", intval($_GET['version']), FALSE);
+    }
+    if (isset($_GET['version'])) {
+        set_ommp_cookie("store.return", $_GET['return'], FALSE);
+    }
+
     // Check if we must display index
     if ($page == "") {
         return [
@@ -87,10 +123,10 @@ function store_process_page($page, $pages_path) {
 
     // Explode the path
     $pages = array_filter(explode("/", $page));
-    $module_name = $pages[0];
+    $module_id = $pages[0];
 
     // Get informations about the module
-    $module_infos = dbGetFirstLineSimple("{$db_prefix}store_modules", "not hidden and not blocked and name = " . $sql->quote($module_name));
+    $module_infos = dbGetFirstLineSimple("{$db_prefix}store_modules", "not hidden and not blocked and name = " . $sql->quote($module_id));
 
     // Check if module exists
     if ($module_infos === FALSE) {
@@ -100,7 +136,7 @@ function store_process_page($page, $pages_path) {
     // Get latest version of module
     $latest_version = dbGetFirstLine("SELECT * FROM {$db_prefix}store_versions WHERE store_id = " . $sql->quote($module_infos['id']) . " ORDER BY version DESC");
 
-    // Check if any version has been updated yet
+    // Check if any version has been uploaded yet
     if ($latest_version === FALSE) {
         return FALSE;
     }
@@ -109,18 +145,93 @@ function store_process_page($page, $pages_path) {
     if (count($pages) == 1) {
 
         // Reads metadate
-        $metadata = @json_decode(@file_get_contents("{$data_path}{$module_name}/meta.json"));
+        $metadata = @json_decode(@file_get_contents("{$data_path}{$module_id}/meta.json"), TRUE);
 
         // Get language from the module
         // Try to load the current user's language, if not available in module then load the default module's language
-        $lang_code = file_exists("{$data_path}{$module_name}/languages/" . $user->lang->current_language() . ".json") ? $user->lang->current_language() : $metadata->default_language;
-        $module_lang = @json_decode(@file_get_contents("{$data_path}{$module_name}/languages/{$lang_code}.json"));
+        $lang_code = file_exists("{$data_path}{$module_id}/languages/" . $user->lang->current_language() . ".json") ? $user->lang->current_language() : $metadata->default_language;
+        $module_lang = @json_decode(@file_get_contents("{$data_path}{$module_id}/languages/{$lang_code}.json"), TRUE);
+
+        // Get informations about the author
+        $author = new User(intval($module_infos['author']));
+
+        // Get available languages
+        $languages_list = [];
+        foreach (scandir("{$data_path}{$module_id}/languages/") as $file) {
+            if (str_ends_with($file, ".json")) {
+                $lang = @json_decode(@file_get_contents("{$data_path}{$module_id}/languages/{$file}"), TRUE);
+                if ($lang) {
+                    $languages_list[] = $lang['@name'];
+                }
+            }
+        }
+        $languages = implode(", ", $languages_list);
+
+        // Get screenshots
+        $screenshots = 0;
+        while (file_exists("{$data_path}{$module_id}/screenshot_{$screenshots}.jpg")) {
+            $screenshots++;
+        }
 
         return [
-            "content" => print_r($latest_version, TRUE) . "<br />" . print_r($metadata, TRUE) . "<br />" . print_r($module_lang, TRUE),
-            "title" => $user->module_lang->get("@module_name")
+            "content" => page_read_module($pages_path . "module_page.html", [
+                "module_name" => htmlvarescape($module_lang['@module_name']),
+                "module_description" => htmlvarescape($module_lang['@module_description']),
+                "module_id" => htmlvarescape($module_id),
+                "module_version" => htmlvarescape($metadata['version']),
+                "author_name" => htmlvarescape($author->longname == "" ? $author->username : $author->longname),
+                "author_username" => htmlvarescape($author->username),
+                "author_certified_image" => $author->certification_html(),
+                "ommp_version" => $latest_version['required'],
+                "module_languages" => $languages,
+                "module_size" => human_file_size(@filesize("{$data_path}{$module_id}/{$module_id}-v{$metadata['version']}.zip"), TRUE, 0),
+                "module_website" => htmlvarescape($metadata['website']),
+                "module_website_hr" => htmlvarescape(substr($metadata['website'], stripos($metadata['website'], '://') + 3)),
+                "module_contact" => htmlvarescape($metadata['contact']),
+                "module_screenshots" => $screenshots
+            ]),
+            "title" => $user->module_lang->get("@module_name") . " - " . $module_lang['@module_name']
         ];
     }
+
+    // Check if we must display an icon
+    if (count($pages) == 2 && $pages[1] == "icon.png") {
+        $icon_path = "{$data_path}{$module_id}/icon.png";
+        // Check if we must resize the image
+        $result = get_image_thumbnail($icon_path, 512);
+        if (!$result) {
+            // If not, display the full image
+            if (file_exists($icon_path)) {
+                header('Content-Type: image/png');
+                readfile($icon_path);
+                exit;
+            }
+            // Else, 404
+            return FALSE;
+        }
+    }
+
+    // Check if we must display a screenshot
+    if (count($pages) == 2 && str_starts_with($pages[1], "screenshot_") && str_ends_with($pages[1], ".jpg") && ctype_digit(substr($pages[1], 11, -4))) {
+        $ss_path = "{$data_path}{$module_id}/$pages[1]";
+        // Check if we must resize the image
+        $result = FALSE;
+        if (isset($_GET['thumb']) && ctype_digit($_GET['thumb'])) {
+            $result = get_image_thumbnail($ss_path, intval($_GET['thumb']));
+        }
+        if (!$result) {
+            // If not, display the full image
+            if (file_exists($ss_path)) {
+                header('Content-Type: image/jpg');
+                readfile($ss_path);
+                exit;
+            }
+            // Else, 404
+            return FALSE;
+        }
+    }
+
+    return FALSE;
 
 }
 
